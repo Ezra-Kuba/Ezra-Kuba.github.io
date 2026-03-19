2b22c6509596484e4c6dbe58f16ccab080513f38
Ezra G Kuba <ezragrey05@gmail.com>
2026-03-18 23:02:06 -0500

    The files for JDCOS

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"<Unnamed>","l":"database_connection"},{"p":"<Unnamed>","l":"gui"},{"p":"<Unnamed>","l":"manager_gui"},{"p":"<Unnamed>","l":"database_connection.QueryResult"}];updateSearchResults();
